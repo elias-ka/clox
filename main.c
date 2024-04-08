d4179493f1ca30c6d49322e4464d941d5a114da5
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-04-08 15:37:29 +0300

    Add tasks.json for running cppcheck on the project

--- main.c
+++ main.c
@@ -25,7 +25,7 @@ static char *read_file(const char *path)
         }
 
         fseek(file, 0L, SEEK_END);
-        const size_t file_size = ftell(file);
+        const int64_t file_size = ftell(file);
         rewind(file);
 
         if (file_size <= 0) {
@@ -41,7 +41,7 @@ static char *read_file(const char *path)
                 exit(74);
         }
 
-        const size_t bytes_read = fread(buffer, sizeof(char), file_size, file);
+        const int64_t bytes_read = fread(buffer, sizeof(char), file_size, file);
         if (bytes_read < file_size) {
                 fprintf(stderr, "Could not read file \"%s\".\n", path);
                 fclose(file);
@@ -49,7 +49,6 @@ static char *read_file(const char *path)
         }
 
         buffer[bytes_read] = '\0';
-        free(buffer);
         fclose(file);
         return buffer;
 }
