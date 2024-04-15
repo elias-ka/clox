67660cbf41008e13af3355a67e62cf45e6caf084
elias-ka <48407293+elias-ka@users.noreply.github.com>
2024-04-15 18:42:27 +0300

    Add support for 'for' loop statements

--- compiler.c
+++ compiler.c
@@ -5,6 +5,7 @@
 #include "object.h"
 #include "scanner.h"
 #include "value.h"
+#include <limits.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -586,6 +587,52 @@ static void expression_statement(void)
         emit_byte(OP_POP);
 }
 
+static void for_statement(void)
+{
+        begin_scope();
+        consume(TOKEN_LEFT_PAREN, "Expect '(' after 'for'.");
+        if (match(TOKEN_SEMICOLON)) {
+                // no initializer
+        } else if (match(TOKEN_VAR)) {
+                var_declaration();
+        } else {
+                expression_statement();
+        }
+
+        size_t loop_start = current_chunk()->size;
+        size_t exit_jump = SIZE_MAX;
+        if (!match(TOKEN_SEMICOLON)) {
+                expression();
+                consume(TOKEN_SEMICOLON, "Expect ';' after loop condition.");
+
+                // jump out of the loop if the condition is false
+                exit_jump = emit_jump(OP_JUMP_IF_FALSE);
+                emit_byte(OP_POP); // condition
+        }
+
+        if (!match(TOKEN_RIGHT_PAREN)) {
+                size_t body_jump = emit_jump(OP_JUMP);
+                size_t increment_start = current_chunk()->size;
+                expression();
+                emit_byte(OP_POP);
+                consume(TOKEN_RIGHT_PAREN, "Expect ')' after for clauses.");
+
+                emit_loop(loop_start);
+                loop_start = increment_start;
+                patch_jump(body_jump);
+        }
+
+        statement();
+        emit_loop(loop_start);
+
+        if (exit_jump != SIZE_MAX) {
+                patch_jump(exit_jump);
+                emit_byte(OP_POP); // condition
+        }
+
+        end_scope();
+}
+
 static void if_statement(void)
 {
         consume(TOKEN_LEFT_PAREN, "Expect '(' after 'if'.");
@@ -672,6 +719,8 @@ static void statement(void)
 {
         if (match(TOKEN_PRINT)) {
                 print_statement();
+        } else if (match(TOKEN_FOR)) {
+                for_statement();
         } else if (match(TOKEN_IF)) {
                 if_statement();
         } else if (match(TOKEN_WHILE)) {
